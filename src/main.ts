982e7d1ecc317429acade2b69cefa56fa50102f3
Ilyak <ilyaprgrmist@gmail.com>
2024-12-22 16:04:29 +0100

    added enviroment variable to start the application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -15,6 +18,11 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 }
+
 bootstrap();
